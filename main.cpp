5a87650abf55df58346deb3360309fa9769701e8
khanhvy-1109 <khanhmeocon98@gmail.com>
2021-06-30 12:27:44 +0700

    up lan dau

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string.h>
+#include "struct.h"
+#include<fstream>
+#include "mylib.h"
+#include "function.h"
+using namespace std;
+
+
+
+int main()
+{
+	int arr1[4999];
+	int arr2[4999];
+	taoMangRd(arr1,1,4999);
+	taoMangRd(arr2,5001,9999);
+	dssv ds;
+	dslop dsl;
+	DSMH dsm;
+	NODECH dsch;
+	Initialize(dsch);
+//	SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE),CONSOLE_FULLSCREEN_MODE,0);
+//	system("mode 1080");
+//	fullscreen();
+//	SetWindowSize(1080,720);
+	mainProcess(dsl,ds,dsm,dsch,arr1,arr2);
+	
+	return 0;
+}
---
+++ struct.h
@@ -0,0 +1,191 @@
+#include<iostream>
+#ifndef _STRUCT_H
+#define _STRUCT_H
+#define MAXMH 100
+#define MAXLOP 500
+#define MAXCAUHOI 200
+#define STACKSIZE 1000
+
+using namespace std;
+// ===============================DANH SACH MON HOC=============================
+struct monHoc
+{
+	string maMH;
+	string tenMH;	
+};
+//danh sách tuy?n tính 
+struct DSMH
+{
+	int somon = 0;
+	monHoc *m[MAXMH] = {NULL};	
+};
+struct CTdethi
+{
+	int sct;
+	int tg;
+	int *bode;
+	int *dapan;
+};
+// ===============================DANH SACH DIEM THI=============================
+struct diemThi
+{
+	string maMH;
+	float diem;
+	CTdethi ctdt;
+};
+struct nodediemThi
+{
+	diemThi info;
+	nodediemThi *pNext = NULL;
+};
+struct dsdiemThi
+{
+	nodediemThi *First = NULL;
+};
+// ===============================DANH SACH SINH VIEN=============================
+struct sinhVien
+{
+	string maSV;
+	string ho;
+	string ten;
+	string gioiTinh;
+	string password;
+	dsdiemThi diem;
+};
+struct nodeSV
+{
+	sinhVien info;
+	nodeSV *pNext = NULL;
+};
+struct dssv
+{
+	nodeSV *First = NULL;
+};
+// ===============================DANH SACH LOP=============================
+struct lop
+{
+	string maLop;
+	string tenLop;
+	string nienKhoa;
+	dssv danhsach;
+};
+struct dslop
+{
+	int solop;
+	lop *l [MAXLOP] = {NULL};
+};
+// ===============================DANH SACH CAU HOI=============================
+struct cauHoi
+{
+	string maMH;
+	string noiDung;
+	string A;
+	string B;
+	string C;
+	string D;
+	string dapAn;
+};
+struct nodecauHoi
+{
+	int id;
+	cauHoi info;
+	nodecauHoi *pLeft = NULL;
+	nodecauHoi *pRight = NULL;
+};
+typedef struct nodecauHoi *NODECH;
+struct dscauhoi
+{
+	nodecauHoi *Root = NULL;
+};
+// ===============================THOI GIAN=============================
+struct TG 
+{
+	int gio;
+	int phut;
+	int giay;
+};
+//==============================================================================================================
+// ===========================================tao cau truc moi===========================================
+// DANH SACH MON HOC THEO CAY NHI PHAN TIM KIEM
+//struct monHoc{
+//	string MAMH;
+//	string TENMH;
+//	DSCH cauHoiMH;
+//};
+//struct dsMonHoc{
+// monHoc mh;
+// struct node *left;
+// struct node *right;
+//};
+//typedef struct dsMonHoc *DSMH;
+//DSMH dsmh = NULL;
+//// DANH SACH CAU HOI THEO DANH SACH TUYEN TINH
+//struct cauHoi
+//{
+//	string noiDung;
+//	string A;
+//	string B;
+//	string C;
+//	string D;
+//	string dapAn;
+//};
+//typedef struct dsCauHoi {
+//	int n;
+//  	cauHoi *listCauHoi[MAXCAUHOI]; // danh sach mang 1 chieu
+//} DSCH;
+//// ===============================DANH SACH LOP=============================
+//struct lop
+//{
+//	string maLop;
+//	string tenLop;
+//	dssv danhsach;
+//};
+//struct dslop
+//{
+//	int solop;
+//	lop *l [MAXLOP] = {NULL};
+//};
+//// ===============================DANH SACH SINH VIEN=============================
+//struct sinhVien
+//{
+//	string maSV;
+//	string ho;
+//	string ten;
+//	string gioiTinh;
+//	string password;
+//	dsdiemThi diem;
+//};
+//struct nodeSV
+//{
+//	sinhVien info;
+//	nodeSV *pNext = NULL;
+//};
+//struct dssv
+//{
+//	nodeSV *First = NULL;
+//};
+//struct CTdethi
+//{
+//	int sct;
+//	int tg;
+//	int *bode;
+//	int *dapan;
+//};
+//// ===============================DANH SACH DIEM THI=============================
+//struct diemThi
+//{
+//	string maMH;
+//	float diem;
+//	CTdethi ctdt;
+//};
+//struct nodediemThi
+//{
+//	diemThi info;
+//	nodediemThi *pNext = NULL;
+//};
+//struct dsdiemThi
+//{
+//	nodediemThi *First = NULL;
+//};
+
+#endif
